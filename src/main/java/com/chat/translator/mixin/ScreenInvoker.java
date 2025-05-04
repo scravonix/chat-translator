fa62650892fc49275360eb4da1204a51a8fa1496
scravonix <scravonix@hotmail.com>
2025-05-04 07:43:57 +0300

    Update ScreenInvoker.java

--- src/main/java/com/chat/translator/mixin/ScreenInvoker.java
+++ src/main/java/com/chat/translator/mixin/ScreenInvoker.java
@@ -7,14 +7,8 @@ import net.minecraft.client.gui.screen.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
-/**
- * Invoker mixin to expose the protected addDrawableChild method on Screen
- */
 @Mixin(Screen.class)
 public interface ScreenInvoker {
-    /**
-     * Exposed Screen.addDrawableChild(Element & Drawable & Selectable)
-     */
     @Invoker("addDrawableChild")
     <T extends Element & Drawable & Selectable> T invokeAddDrawableChild(T widget);
 }
